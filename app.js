3d56f5819bfc5c6de0ff9cac8db7810dbed451ff
CyberWarJBR <cyberjbr@gmail.com>
2018-01-05 00:26:35 +0700

    wew

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const https = require('https');
 const options = {
   hostname: 'api.github.com',
   port: 443,
-  path: '/users/yofri',
+  path: '/users/cyberwarjbr',
   method: 'GET',
   headers: {
   	'user-agent' : 'nodejs'
@@ -15,19 +15,27 @@ const options = {
 
 // TODO : read data
 let request = https.request(options, (response) => {
-  // console.log('statusCode:', response.statusCode);
-  // console.log('headers:', response.headers);
 
-  response.on('data', (d) => {
-    process.stdout.write(d)
+  let body = ''
+
+  response.on('data', (data) => {
+    body = body + data
   })
 
-// TODO : patse data
-// TODO : print data out
+  response.on('end', () => {
 
+// TODO : patse data
+
+let profile = JSON.parse(body)
+console.log(profile.id)
+})
+  // TODO : print data out
+  
+  
 })
 
 request.on('error', (e) => {
   console.error(e)
 })
+
 request.end()
