828d19ff86879a4919a7090849b37060420f90da
CyberWarJBR <cyberjbr@gmail.com>
2018-01-05 01:37:37 +0700

    done

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
 
-//TODO : connect to github API
-  
-const https = require('https');
+let profile = require('./profile.js')
 
-const options = {
-  hostname: 'api.github.com',
-  port: 443,
-  path: '/users/cyberwarjbr',
-  method: 'GET',
-  headers: {
-  	'user-agent' : 'nodejs'
-  }
-}
+// let profiles =['rizafahmi', 'yofri', 'cyberwarjbr']
+// profiles.map(user =>{
+// 	return profile.get(user)
+// })
+// 
 
-// TODO : read data
-let request = https.request(options, (response) => {
-
-  let body = ''
-
-  response.on('data', (data) => {
-    body = body + data
-  })
-
-  response.on('end', () => {
-
-// TODO : patse data
-
-let profile = JSON.parse(body)
-console.log(profile.id)
-})
-  // TODO : print data out
-  
-  
-})
-
-request.on('error', (e) => {
-  console.error(e)
-})
-
-request.end()
+let profiles = process.argv.slice(2)
+profiles.map(profile.get)
